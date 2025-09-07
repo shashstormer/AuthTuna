3c0a3f4f35c3558a0d1adccc67851f50d2711802
Shashank A Patil <shashanka5398@gmail.com>
2025-09-07 16:00:37 +0530

    feat: integrate authentication and social routers into the application

--- authtuna/__init__.py
+++ authtuna/__init__.py
@@ -6,16 +6,17 @@ __version__ = "0.0.1"
 __author__ = "shashstormer"
 __description__ = "A robust, multi-layered security foundation for modern web applications"
 
-# from .core.authorizer import Authorizer
-# from .core.security import SecurityManager
-from .core.database import DatabaseManager
+from .core.database import DatabaseManager, db_manager, User
 from .core.config import settings
-# from .helpers.mailer import Mailer
+from .middlewares.session import DatabaseSessionMiddleware
+from .routers import auth_router, social_router
 
 __all__ = [
-    # "Authorizer",
-    # "SecurityManager",
     "DatabaseManager",
     "settings",
-    # "Mailer"
+    "db_manager",
+    "User",
+    "DatabaseSessionMiddleware",
+    "auth_router",
+    "social_router",
 ]
---
+++ authtuna/routers/__init__.py
@@ -0,0 +1,2 @@
+from authtuna.routers.auth import router as auth_router
+from authtuna.routers.social import router as social_router
