1ad863f74333a46b1f599f38b4adde000e59a25e
Shashank A Patil <shashanka5398@gmail.com>
2025-10-10 03:51:02 +0530

    add optional user retrieval and IP address functions to fastapi integration

--- authtuna/integrations/__init__.py
+++ authtuna/integrations/__init__.py
@@ -1,1 +1,1 @@
-from authtuna.integrations.fastapi_integration import PermissionChecker, get_current_user, auth_service, RoleChecker
+from authtuna.integrations.fastapi_integration import PermissionChecker, get_current_user, auth_service, RoleChecker, get_current_user_optional, get_user_ip
