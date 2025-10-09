9d81c5c82229d31c9736b1b46016760eade2d0c6
Shashank A Patil <shashanka5398@gmail.com>
2025-10-10 03:29:05 +0530

    bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="authtuna",
-    version="0.1.6",
+    version="0.1.7",
     author="shashstormer",
     author_email="shashanka5398@gmail.com",
     description="A high-performance, framework-agnostic authorization and session management library for Python",
