d5bd2ae291651f39638430d1e535dba2594eac59
Shashank A Patil <shashanka5398@gmail.com>
2025-11-02 00:02:40 +0530

    chore: comment out early initialization of encryption utilities in __init__.py

--- authtuna/core/__init__.py
+++ authtuna/core/__init__.py
@@ -1,6 +1,6 @@
 from .config import settings, init_settings
 # from .database import db_manager
-from .encryption import encryption_utils
+# from .encryption import encryption_utils -> was initializing settings wayy to early so commented this.
 from .exceptions import *
 from .mfa import MFAManager
 from .social import get_social_provider
